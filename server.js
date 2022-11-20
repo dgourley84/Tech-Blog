3d93c83da80d7bd194948e5ec0ce9b81d6d4168d
dgourley84 <81537606+dgourley84@users.noreply.github.com>
2022-11-20 20:19:30 +1000

    Set up of serverjs

---
+++ server.js
@@ -0,0 +1,22 @@
+const app = express();
+const PORT = process.env.PORT || 3002;
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+const sess = {
+    secret: 'Super secret secret',
+    cookie: { maxAge: 300000 },
+    resave: false,
+    saveUninitialized: true,
+    store: new SequelizeStore({
+        db: sequelize
+    })
+};
+app.use(session(sess));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+app.use(routes);
+sequelize.sync({ force: false }).then(() => {
+    app.listen(PORT, () => console.log('Now listening'));
+});
